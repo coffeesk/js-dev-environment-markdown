2e8960ac80187d132c9279fe26195837d50b5073
Martin Galik <matogalik@gmail.com>
2021-09-12 08:44:02 +0200

    ok

--- scripts/upload.js
+++ scripts/upload.js
@@ -8,4 +8,4 @@ const fileUrl = `fmp://${server}/${file}?script=${uploadScript}&param=`;
 
 const thePath = path.join(__dirname, "../", "dist", "index.html");
 const url = fileUrl + encodeURIComponent(thePath);
-open(url);
+open(url); //bbv
